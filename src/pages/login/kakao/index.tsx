690f9bb119aeee4e28fe1e80f10b5b5bee60e3df
roddywhite <roddywhite1111@gmail.com>
2023-01-01 13:26:19 +0900

    feat: 카카오 로그인 인가코드 파싱

--- src/pages/login/kakao/index.tsx
+++ src/pages/login/kakao/index.tsx
@@ -1,7 +1,11 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const KaKaoLogin = () => {
-  return <div>
-      로딩중입니다... 곧 로그인이 완료됩니다...
-  </div>;
+  const url = new URL(window.location.href);
+  const code = url.searchParams.get('code');
+  console.log(code);
+  return <div>로딩중입니다... 곧 로그인이 완료됩니다...</div>;
 };
 
 export default KaKaoLogin;
