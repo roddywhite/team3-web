ca39ff9ebcdfb6f8055435d931198675f56f485c
roddywhite <roddywhite1111@gmail.com>
2023-01-18 13:24:31 +0900

    feat: 랜딩페이지 검색바 추가

--- src/pages/market/components/search-bar/index.tsx
+++ src/pages/market/components/search-bar/index.tsx
@@ -1,12 +1,15 @@
-import { Bar, Div, Img } from './search-bar.styled';
+import { Wrapper, Bar, Div, Img, Button } from './search-bar.styled';
 import search from '../../../../assets/search.svg';
 
 const SearchBar = () => {
   return (
-    <Div>
-      <Img src={search} />
-      <Bar placeholder="봉천동 근처 물품 검색하기" />
-    </Div>
+    <Wrapper>
+      <Div>
+        <Img src={search} />
+        <Bar placeholder="봉천동 근처 물품 검색하기" />
+      </Div>
+      <Button>검색</Button>
+    </Wrapper>
   );
 };
 
